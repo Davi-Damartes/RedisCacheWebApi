5faa2ac5794cbb2c2a321f2688dedb0c9f5a35eb
Davi Lima <davidamartesgit@Gmail.com>
2024-07-19 19:03:54 -0300

    CRUD através do controller

---
+++ Controllers/JogadorController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiCaching.Data;
+using WebApiCaching.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiCaching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JogadorController : ControllerBase
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public JogadorController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<Jogador>> ObterJogadores( )
+        {
+            var result = await _appDbContext.Jogadores.ToListAsync();
+
+            if ( result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+  
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Jogador>> ObterJogador (int id)
+        {
+            var result = await _appDbContext.Jogadores.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (result == null)
+                return NotFound();
+
+            return Ok(result);
+        }
+
+    
+        [HttpPost]
+        public async Task<IActionResult> AdicionarJogardor([FromBody] Jogador jogador)
+        {
+            if(jogador == null)
+            {
+                return BadRequest();
+            }
+
+            await _appDbContext.Jogadores.AddAsync(jogador);
+            await _appDbContext.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> ExcluirJogador(int id)
+        {
+            var result = await _appDbContext.Jogadores.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (result == null)
+                return NotFound();
+
+            await _appDbContext.Jogadores.Where(x => x.Id == id).ExecuteDeleteAsync();
+            await _appDbContext.SaveChangesAsync();
+            return Ok(result);
+        }
+    }
+}
