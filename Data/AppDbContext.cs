4bbbdf66cee61b40b04f2bf144ef0aea8c3f51bf
Davi Lima <davidamartesgit@Gmail.com>
2024-07-19 19:03:40 -0300

    Configuração do Banco de dados com EfCore

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Any;
+using WebApiCaching.Models;
+
+namespace WebApiCaching.Data
+{
+    public class AppDbContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public AppDbContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_configuration.GetConnectionString("ConnectionString"));
+        }
+
+        public DbSet<Jogador> Jogadores { get; set;}
+    }
+}
