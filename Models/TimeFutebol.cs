10c77e70bf32d61fe6c5fb0002b54071344d7f6a
Davi Lima <davidamartesgit@Gmail.com>
2024-07-20 16:36:10 -0300

    Classe responsável pelo Times

---
+++ Models/TimeFutebol.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApiCaching.Models
+{
+    public class TimeFutebol
+    {
+        public int TimeFutebolId { get; set; }  // Identificador único do Time
+        public string Nome { get; set; } = string.Empty;  // Nome do Time
+        public string Descricao { get; set; } = string.Empty;  // Descrição do Time
+        public string Classificacao { get; set; } = string.Empty;  // Classificação do Time
+
+        // Relação de um-para-muitos com Jogador
+        public List<Jogador>? Jogadores { get; set; } = new List<Jogador>();
+    }
+
+
+}
