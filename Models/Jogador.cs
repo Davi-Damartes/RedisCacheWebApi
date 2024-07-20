32af94b3a5dd092764f27187a2a92c07a021a74f
Davi Lima <davidamartesgit@Gmail.com>
2024-07-20 16:36:25 -0300

    Add propriedades

--- Models/Jogador.cs
+++ Models/Jogador.cs
@@ -1,9 +1,17 @@
-﻿namespace WebApiCaching.Models
+﻿using WebApiCaching.Models;
+
+namespace WebApiCaching.Models
 {
     public class Jogador
     {
-        public int Id { get; set; }
-        public string Nome { get; set; } = string.Empty;
-        public int NumeroCamisa { get; set; } 
+        public int Id { get; set; }  // Identificador único do Jogador
+        public string Nome { get; set; } = string.Empty;  // Nome do Jogador
+        public int NumeroCamisa { get; set; }  // Número da camisa do Jogador
+
+        // Chave estrangeira para a tabela TimeFutebol
+        public int TimeFutebolId { get; set; }
+        // Navegação para TimeFutebol
+        public TimeFutebol? TimeFutebol { get; set; }
     }
+
 }
