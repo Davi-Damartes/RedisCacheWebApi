90566f5dc78d102c595240a1afc362ea393a71d2
Davi Lima <davidamartesgit@Gmail.com>
2024-07-21 18:50:19 -0300

    Dtos para a classe Jogador

---
+++ Dtos/JogadorDto.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApiCaching.Dtos
+{
+    public class JogadorDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public int NumeroCamisa { get; set; }
+
+        public int TimeFutebolId { get; set; }
+        public TimeFutebolDto TimeFutebol { get; set; }
+    }
+}
