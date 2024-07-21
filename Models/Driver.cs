76ed894b8ca79807aa01a80e85d9cdf8f5c57936
Davi Lima <davidamartesgit@Gmail.com>
2024-07-21 18:49:10 -0300

    Add classe Driver

---
+++ Models/Driver.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiCaching.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string DriverNb { get; set; } = string.Empty;
+    }
+}
