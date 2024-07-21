b01fbd166f1f092f2f1a3eef44da8ffc0d88d9a6
Davi Lima <davidamartesgit@Gmail.com>
2024-07-21 18:48:38 -0300

    Removendo comentários

--- Models/TimeFutebol.cs
+++ Models/TimeFutebol.cs
@@ -2,12 +2,11 @@
 {
     public class TimeFutebol
     {
-        public int TimeFutebolId { get; set; }  // Identificador único do Time
-        public string Nome { get; set; } = string.Empty;  // Nome do Time
-        public string Descricao { get; set; } = string.Empty;  // Descrição do Time
-        public string Classificacao { get; set; } = string.Empty;  // Classificação do Time
+        public int TimeFutebolId { get; set; }
+        public string Nome { get; set; } = string.Empty; 
+        public string Descricao { get; set; } = string.Empty; 
+        public string Classificacao { get; set; } = string.Empty;  
 
-        // Relação de um-para-muitos com Jogador
         public List<Jogador>? Jogadores { get; set; } = new List<Jogador>();
     }
 
