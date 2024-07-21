53f5ea0360adffa31f10bba18709a40d98cedf2e
Davi Lima <davidamartesgit@Gmail.com>
2024-07-21 18:48:53 -0300

    Removendo comentários

--- Models/Jogador.cs
+++ Models/Jogador.cs
@@ -1,17 +1,14 @@
-﻿using WebApiCaching.Models;
-
-namespace WebApiCaching.Models
+﻿namespace WebApiCaching.Models
 {
-    public class Jogador
-    {
-        public int Id { get; set; }  // Identificador único do Jogador
-        public string Nome { get; set; } = string.Empty;  // Nome do Jogador
-        public int NumeroCamisa { get; set; }  // Número da camisa do Jogador
+        public class Jogador
+        {
+            public int Id { get; set; }  
+            public string Nome { get; set; } = string.Empty;  
+            public int NumeroCamisa { get; set; } 
 
-        // Chave estrangeira para a tabela TimeFutebol
-        public int TimeFutebolId { get; set; }
-        // Navegação para TimeFutebol
-        public TimeFutebol? TimeFutebol { get; set; }
-    }
+            public int TimeFutebolId { get; set; }
+
+            public TimeFutebol? TimeFutebol { get; set; }
+        }
 
 }
