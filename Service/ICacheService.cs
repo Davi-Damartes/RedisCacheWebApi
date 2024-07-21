630502b5de41035e07fc0f823d64600693eff41b
Davi Lima <davidamartesgit@Gmail.com>
2024-07-21 18:52:17 -0300

    Interface com métodos para armazernar, retornar e excluir dados em caching

---
+++ Service/ICacheService.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiCaching.Service
+{
+    public interface ICacheService
+    {
+        T GetData<T>(string Key);
+
+        bool SetData<T>(string Key, T value, DateTimeOffset expirationTime);
+
+        object RemoveData(string Key);
+    }
+}
