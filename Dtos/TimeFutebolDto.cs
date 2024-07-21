4cf7f39423b0789de5ea1ce9fd4859bc983da818
Davi Lima <davidamartesgit@Gmail.com>
2024-07-21 18:50:29 -0300

    Dtos para a classe TimeFutebol

---
+++ Dtos/TimeFutebolDto.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApiCaching.Dtos
+{
+    public class TimeFutebolDto
+    {
+    //    public int TimeFutebolId { get; set; }
+        public string Nome { get; set; } = string.Empty;
+        public string Descricao { get; set; } = string.Empty; 
+        public string Classificacao { get; set;} = string.Empty;
+        //public List<JogadorDto> Jogadores { get; set; } =new List<JogadorDto>();
+    }
+}
