ebdfd855ba15fae4bef03adf87261f5cf80e7405
Davi Lima <davidamartesgit@Gmail.com>
2024-07-22 20:47:41 -0300

    Interface para Resitório do Time Futebol

---
+++ Repository/ITimeFutebolRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using WebApiCaching.Models;
+
+namespace WebApiCaching.Repository
+{
+    public interface ITimeFutebolRepository
+    {
+        Task<TimeFutebol> ObterTime(int id);
+        Task<IEnumerable<TimeFutebol>> ObterTimes();
+        Task<bool> AddTime(TimeFutebol timeFutebol);
+        Task<bool> ExcluirTime(int id);
+    }
+}
