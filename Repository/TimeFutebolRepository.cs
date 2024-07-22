cc793e4d92c921c63c581a1bc29e52e2bb03c0f5
Davi Lima <davidamartesgit@Gmail.com>
2024-07-22 20:48:36 -0300

    Métodos do Repositório

---
+++ Repository/TimeFutebolRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiCaching.Data;
+using WebApiCaching.Models;
+
+namespace WebApiCaching.Repository
+{
+    public class TimeFutebolRepository : ITimeFutebolRepository
+    {
+        private readonly AppDbContext _context;
+
+        public TimeFutebolRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<TimeFutebol>> ObterTimes( )
+        {
+            return await _context.TimeFutebols
+                                .Include(x => x.Jogadores)
+                                .AsNoTracking().ToListAsync();
+        }
+
+        public async Task<TimeFutebol> ObterTime(int id)
+        {
+            return await _context.TimeFutebols
+                                .Include(x => x.Jogadores)
+                                .AsNoTracking()
+                                .FirstOrDefaultAsync(x => x.TimeFutebolId == id) ?? null!;
+
+        }
+
+
+        public async Task<bool> AddTime(TimeFutebol timeFutebol)
+        {
+            var timeExiste = await ObterTime(timeFutebol.TimeFutebolId);
+            if (timeExiste != null)
+            {
+                return false;
+            }
+
+            await _context.AddAsync(timeFutebol);
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<bool> ExcluirTime(int id)
+        {
+            var timeExistente = await ObterTime(id);
+            if (timeExistente != null)
+            {
+                _context.Remove(timeExistente);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            return false;
+        }
+    }
+}
