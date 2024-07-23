0c2bc1aafb55d292a399d593929e2705e707c5ff
Davi Lima <davidamartesgit@Gmail.com>
2024-07-23 20:38:32 -0300

    Add pasta aos arquivos e modificando métodos

---
+++ Repository/JogadorRepositories/IJogadorRepository.cs
@@ -0,0 +1,18 @@
+﻿using WebApiCaching.Models;
+
+namespace WebApiCaching.Repository.JogadorRepositories
+{
+    public interface IJogadorRepository
+    {
+        Task<IEnumerable<Jogador>> ObterJogadores();
+
+        Task<Jogador> ObterJogador(int Id);
+
+        Task<bool> AddJogador(Jogador Jogador);
+
+        Task<Jogador> TransferirJogador(int Id, int TimeId);
+
+        Task<bool> ExcluirJogador(int Id);
+
+    }
+}
