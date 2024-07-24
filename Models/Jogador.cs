7eb11a3947df736d6784e5493d7f3566adca1de6
Davi Lima <davidamartesgit@Gmail.com>
2024-07-24 18:51:51 -0300

    Trocando Nome

--- Models/Jogador.cs
+++ Models/Jogador.cs
@@ -5,10 +5,8 @@
             public int Id { get; set; }  
             public string Nome { get; set; } = string.Empty;  
             public int NumeroCamisa { get; set; } 
-
             public int TimeFutebolId { get; set; }
-
-            public TimeFutebol? TimeFutebol { get; set; }
+            public TimeFutebol? Time { get; set; }
         }
 
 }
