8e1ca3e0d9139347825465dfe307230271c1f2ab
Davi Lima <davidamartesgit@Gmail.com>
2024-07-24 18:55:06 -0300

    Trocando e ajustando propriedades para Mapeamento do TimeFutebol

--- MappingEntity/MappingTimeFutebol.cs
+++ MappingEntity/MappingTimeFutebol.cs
@@ -9,16 +9,14 @@ namespace WebApiCaching.MappingEntity
         {
             return new TimeFutebol
             {
-                TimeFutebolId = timeFutebolDto.TimeFutebolId,
+                Id = timeFutebolDto.TimeFutebolId,
                 Nome = timeFutebolDto.Nome,
-                Descricao = timeFutebolDto.Descricao,
                 Classificacao = timeFutebolDto.Classificacao,
-                Jogadores = timeFutebolDto.Jogadores?.Select(jogado => new Jogador
+                Jogadores = timeFutebolDto.Jogadores?.Select(jogador => new Jogador
                 {
-                    Id = jogado.Id,
-                    Nome = jogado.Nome,
-                    NumeroCamisa = jogado.NumeroCamisa,
-                    //TimeFutebolId = jogado.TimeFutebolId,
+                    Id = jogador.Id,
+                    Nome = jogador.NomeJogador,
+                    NumeroCamisa = jogador.NumeroCamisa
 
                 }).ToList()
             };
@@ -27,17 +25,35 @@ namespace WebApiCaching.MappingEntity
         {
             return new TimeFutebolDto
             {
-                TimeFutebolId = timeFutebol.TimeFutebolId,
+                TimeFutebolId = timeFutebol.Id,
                 Nome = timeFutebol.Nome,
-                Descricao = timeFutebol.Descricao,
                 Classificacao = timeFutebol.Classificacao,
-                Jogadores = timeFutebol.Jogadores?.Select(jogado => new JogadorDto
+                Jogadores = timeFutebol.Jogadores?.Select(jogador => new JogadorDto
                 {
-                    Id = jogado.Id,
-                    Nome = jogado.Nome,
-                    NumeroCamisa = jogado.NumeroCamisa,
-                    //TimeFutebolId = jogado.TimeFutebolId,
+                    Id = jogador.Id,
+                    NomeJogador = jogador.Nome,
+                    NumeroCamisa = jogador.NumeroCamisa,
+                    NomeTime = jogador.Time.Nome,
+                    TimeFutebolId = jogador.TimeFutebolId,
 
+                }).ToList()
+            };
+        } 
+        
+        public static TimeFutebol ConverterTimeFutEditParaTimeFut(this TimeFutebolEditDto novoTime, TimeFutebol time)
+        {
+            return new TimeFutebol
+            {
+                Id = time.Id,
+                Nome = novoTime.Nome,
+                Classificacao = novoTime.Classificacao,
+                Jogadores = time.Jogadores.Select(jogador => new Jogador
+                {
+                    Id = jogador.Id,
+                    Nome = jogador.Nome,
+                    NumeroCamisa = jogador.NumeroCamisa,
+                    TimeFutebolId = jogador.TimeFutebolId,
+                    
                 }).ToList()
             };
         } 
@@ -47,16 +63,16 @@ namespace WebApiCaching.MappingEntity
             return (from time in timesFutebol
                     select new TimeFutebolDto
                     {
-                        TimeFutebolId = time.TimeFutebolId,
+                        TimeFutebolId = time.Id,
                         Nome = time.Nome,
-                        Descricao = time.Descricao,
                         Classificacao = time.Classificacao,
                         Jogadores = time.Jogadores?.Select(jogador => new JogadorDto
                         {
                             Id = jogador.Id,
-                            Nome = jogador.Nome,
+                            NomeJogador = jogador.Nome,
                             NumeroCamisa = jogador.NumeroCamisa,
-                            //TimeFutebolId = jogador.TimeFutebolId,
+                            NomeTime = jogador.Time.Nome,
+                            TimeFutebolId = jogador.TimeFutebolId,
 
                         }).ToList()
                     });
